bb48dbd694345626a39f0ce9dbecddb7a3b541b1
Hayden Shane <141196705+HaydoShane@users.noreply.github.com>
2024-04-15 17:01:53 +0800

    Restructured js file

--- script.js
+++ script.js
@@ -1,32 +1,23 @@
 // 1. Locate the Element(s)
-let square = document.getElementById('square')
+let square = document.getElementById('square');
+let words = document.getElementById('words');
 
 // 2. Write the functionality needed when the event occurs
-function changeColour(colour){
-  square.style.background = colour;
+function changeColour(colour) {
+    square.style.background = colour;
 }
 
-// 3. Link the element and event to the functionality
-//    i.e add an event listener
-square.addEventListener('click', (event) => showMessage());
-square.addEventListener('mouseover', (event) => changeColour('grey'));
-square.addEventListener('mouseout', (event) => changeColour('red'));
-
-let words = document.getElementById('words')
-
-function greeting(){
+function greeting() {
     console.log("Welcome to Flatland");
-    words.innerHTML = "Welcome to Flatland. <br> I am Square!";
+    words.innerHTML = "Welcome to Flatland.<br> I am Square!";
 }
-document.onload = greeting()
 
-function showMessage(){
+function showMessage() {
     let message = "Build a " + createBuzzwordPhrase();
     words.innerHTML = message;
 }
 
 function createBuzzwordPhrase() {
-    /* See https://en.wikipedia.org/wiki/List_of_buzzwords */
     let buzz = ["Paradigm-changing", "Multi-tier", "10,000-foot", "Agile", "Customer", "Win-win"];
     let action = ["empowered", "value-added", "synergy", "creative", "oriented", "focused", "aligned"];
     let outcome = ["process", "deliverable", "solution", "tipping-point", "strategy", "vision"];
@@ -35,5 +26,13 @@ function createBuzzwordPhrase() {
     let idx_act = Math.floor(Math.random() * action.length);
     let idx_out = Math.floor(Math.random() * outcome.length);
 
-    return  buzz[idx_buz] + " " + action[idx_act] + " " + outcome[idx_out];
+    return buzz[idx_buz] + " " + action[idx_act] + " " + outcome[idx_out];
 }
+
+// 3. Link the element and event to the functionality
+//    i.e add an event listener
+square.addEventListener('click', (event) => showMessage());
+square.addEventListener('mouseover', (event) => changeColour('grey'));
+square.addEventListener('mouseout', (event) => changeColour('red'));
+
+document.onload = greeting();
